860c6d4d16d809005a234f3f2867361cbb6a4094
Oscar <oscfriberg@gmail.com>
2019-10-28 20:59:16 +0100

    working

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDaxUINf69dWljXYQK7wjYWctyFUUz7SnA",
+    authDomain: "cra-firebase-56b65.firebaseapp.com",
+    databaseURL: "https://cra-firebase-56b65.firebaseio.com",
+    projectId: "cra-firebase-56b65",
+    storageBucket: "cra-firebase-56b65.appspot.com",
+    messagingSenderId: "456755966986",
+    appId: "1:456755966986:web:6dad27afd74039cee99bf3",
+    measurementId: "G-PM21VBBZE8"
+};
